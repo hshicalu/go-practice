2b85e109bf091621049ea758aa6bfa9fd46f7c57
hicalu <hshicalu@gmail.com>
2022-11-06 11:40:14 +0900

    update goroutine

--- goroutine.go
+++ goroutine.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 	"time"
 )
 
@@ -19,7 +20,15 @@ func getLuckyNum() {
 
 func main() {
 	fmt.Println("what is today's lucky num?")
-	go getLuckyNum()
-
+	// go getLuckyNum()
 	// time.Sleep(time.Second * 5)
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+		getLuckyNum()
+	}()
+	wg.Wait()
 }
